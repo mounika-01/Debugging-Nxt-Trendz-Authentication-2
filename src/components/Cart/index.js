70c1493ee7a8073e30b96a96ae6a7b5c40b239fb
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-11-14 17:17:39 +0530

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,15 +1,15 @@
-import {Redirect} from 'react-router-dom'
-import Cookies from 'js-cookie'
+import { Navigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
-import Header from '../Header'
-
-import './index.css'
+import Header from '../Header';
+import './index.css';
 
 const Cart = () => {
-  const accessToken = Cookies.get('jwt_token')
-
-  if (accessToken === undefined) {
-    return <Redirect to="/login" />
+  const accessToken = Cookies.get('jwt_token');
+  
+  // Redirect to login if there's no access token
+  if (!accessToken) {
+    return <Navigate to="/login" />;
   }
 
   return (
@@ -23,7 +23,7 @@ const Cart = () => {
         />
       </div>
     </>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
