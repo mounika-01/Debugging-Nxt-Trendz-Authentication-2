5d61281c92e79b1886116a43b058b57fc4bbc0fd
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-11-14 17:21:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 // App.js
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
-import Cookies from 'js-cookie'
-import LoginRoute from './LoginRoute'
-import HomeRoute from './HomeRoute'
-import ProductsRoute from './ProductsRoute'
-import CartRoute from './CartRoute'
-import Logout from './Logout'
+import {Switch, Route} from 'react-router-dom'
 
-const ProtectedRoute = ({component: Component, ...rest}) => {
-  const jwtToken = Cookies.get('jwt_token')
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        jwtToken ? <Component {...props} /> : <Redirect to="/login" />
-      }
-    />
-  )
-}
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+
+import './App.css'
 
 const App = () => (
-  <Router>
-    <Switch>
-          <Route exact path="/login" component={LoginRoute} />
-    <ProtectedRoute exact path="/" component={HomeRoute} />
-    <ProtectedRoute exact path="/products" component={ProductsRoute} />
-    <ProtectedRoute exact path="/cart" component={CartRoute} />
-
-    </Switch>
-  </Router>
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/products" component={Products} />
+    <Route exact path="/cart" component={Cart} />
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App
