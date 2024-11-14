1b65650ae812c76eb8bb9d36f3dd893a072f3e13
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-11-14 17:19:08 +0530

    Update index.js

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -113,7 +113,6 @@ class LoginForm extends Component {
           alt="website login"
         />
         <form className="form-container" onSubmit={this.submitForm}>
-
           <img
             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
             className="login-website-logo-desktop-img"
